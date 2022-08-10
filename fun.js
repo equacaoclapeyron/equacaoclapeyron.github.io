fd38a0901577d6524f623d57fcbddaf8ee718f34
equacaoclapeyron <111002666+equacaoclapeyron@users.noreply.github.com>
2022-08-10 13:44:40 -0300

    Add files via upload

---
+++ fun.js
@@ -0,0 +1,8 @@
+
+function calcular_elementos(x){
+    var mol = document.getElementById("mols").value;
+    var temperatura = document.getElementById("temperatura").value;
+    var volume = document.getElementById("volume").value;
+
+    document.write("<h1> Valores: " + mol + temperatura + volume + "</h1>");
+}
