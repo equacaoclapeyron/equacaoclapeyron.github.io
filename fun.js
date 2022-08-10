72227dc6295af165638811ef5830d5adfd517d99
equacaoclapeyron <111002666+equacaoclapeyron@users.noreply.github.com>
2022-08-10 15:20:25 -0300

    Delete fun.js

--- fun.js
+++
@@ -1,8 +0,0 @@
-
-function calcular_elementos(x){
-    var mol = document.getElementById("mols").value;
-    var temperatura = document.getElementById("temperatura").value;
-    var volume = document.getElementById("volume").value;
-
-    document.write("<h1> Valores: " + mol + temperatura + volume + "</h1>");
-}
